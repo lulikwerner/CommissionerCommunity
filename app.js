8f340f3f55054b61f932e10c884e986adda0e4ba
lulikwerner <lulikwerner@gmail.com>
2026-03-11 23:01:37 -0300

    port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3007;
 
 // Configuración de Handlebars
 const hbs = exphbs.create({
